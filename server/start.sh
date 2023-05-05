dfc9996604edb94cb4edbdda7db33d4c4cdaca0b
ZekeZ <contact@zekez.dev>
2023-05-05 19:08:51 +1000

    fix server scripts

--- server/start.sh
+++ server/start.sh
@@ -2,7 +2,7 @@
 
 VERSION="develop"
 MINECRAFT="1.19.2"
-REPO="https://raw.githubusercontent.com/Warped-Modding/zapp"
+REPO="https://raw.githubusercontent.com/Warped-Modding/Warp-Core"
 
-java -jar packwiz-installer-bootstrap.jar -g -s server $REPO/$VERSION/pack/$MINECRAFT/pack.toml
+java -jar packwiz-installer-bootstrap.jar -g -s server $REPO/$VERSION/$MINECRAFT/pack.toml
 java -Xms4G -Xmx4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar quilt-server-launch.jar --nogui
